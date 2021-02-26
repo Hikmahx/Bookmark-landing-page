1a50268ee6613f899e88303e7758759f6acd68a7
Hikmahx <hikmayousuph@gmail.com>
2021-02-25 18:16:09 -0800

    js for tabs completed

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const hamburger = document.querySelector('.hamburger-menu'),
       navMenu = document.querySelector('.nav-menu');
       social = document.querySelector('.social'); 
 
+      tabHeader = document.querySelectorAll(".tab-header");
+      tabLinks = document.querySelectorAll(".tablink");
+      tabContent = document.querySelectorAll(".tab-content");
+
 // Menu toggle event
 hamburger.addEventListener('click', (e)=>{
   header.classList.toggle("open");
@@ -11,3 +15,18 @@ hamburger.addEventListener('click', (e)=>{
   navMenu.classList.toggle("open");
   social.classList.toggle("open");
 })
+
+
+// TAB CLICK EVENT 
+for (let number = 0; number < tabLinks.length; number++) {
+  tabLinks[number].addEventListener('click', ()=>{
+      tabLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+      tabContent.forEach(content => {
+        content.classList.remove('active');
+      });
+      tabLinks[number].classList.add('active');
+      tabContent[number].classList.add('active');
+  })
+}
